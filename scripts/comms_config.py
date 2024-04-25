9ee202c7037f799748f76c72e6d19ee94af9de34
BillyMazotti <womazotti@gmail.com>
2024-04-25 00:26:02 +0000

    Added scripts from older repo history

---
+++ scripts/comms_config.py
@@ -0,0 +1,2 @@
+joints_positions_json_path = "/run/user/1000/gvfs/sftp:host=billys-macbook-pro-5.local/Users/billymazotti/github/OSAM-Project/joints_positions.json"
+ee_velocity_json_path = "/run/user/1000/gvfs/sftp:host=billys-macbook-pro-5.local/Users/billymazotti/github/OSAM-Project/ee_velocity.json"
